57a135b0be76474e280245bc360ad0b40e3c6efd
chngyicheng <yiichengg@gmail.com>
2021-12-09 21:47:22 +0800

    Added backup lktrack file

--- src/old_video_lktrack.py
+++ src/old_video_lktrack.py
@@ -29,31 +29,28 @@ class LKTracker(object):
 		self.prev_features   = []
 		self.tracks          = []
 		self.current_frame   = 0
-		self.detect_interval = 3
+		self.detect_interval = 5
 		self.current_x       = 0
 		self.current_y       = 0
-		self.color = np.random.randint(0,255,(100,3))
-
 
 		## For thresholding
-		self.threshold      = 60  # BINARY threshold
-		self.blurValue      = 5  # GaussianBlur parameter
+		self.threshold = 60  # BINARY threshold
+		self.blurValue = 5  # GaussianBlur parameter
 		self.bgSubThreshold = 50
-		self.learningRate   = 0.003
-		self.isBgCaptured   = 0   # whether the background captured
-		self.bgModel        = None
-		self.objectDetected = False
+		self.learningRate = 0.007
+		self.isBgCaptured = 0   # whether the background captured
+		self.bgModel = None
 		
 	def detectPoints(self):
 		"""Detect 'Good features to track' (corners)
 		in current frame using sub-pixel accuracy"""
 
 		# Load image and threshold image
-		self.capture   = cv2.VideoCapture(self.imnames)
-		width          = int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-		height         = int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-		self.fps       = int(self.capture.get(cv2.CAP_PROP_FPS))
-		self.writer    = cv2.VideoWriter('drone3_update_track.mp4', cv2.VideoWriter_fourcc(*'XVID'),25, (width, height))
+		self.capture = cv2.VideoCapture(self.imnames)
+		width = int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+		height = int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+		self.fps = int(self.capture.get(cv2.CAP_PROP_FPS))
+		# self.writer = cv2.VideoWriter('drone_track_2.mp4', cv2.VideoWriter_fourcc(*'XVID'),25, (width, height))
 		__, self.frame = self.capture.read()
 		for i in range(5):
 			__, self.frame = self.capture.read()
@@ -66,72 +63,63 @@ class LKTracker(object):
 		cv2.cornerSubPix(self.drawing, self.prev_features, **subpix_params)
 
 		self.features = self.prev_features
-		print(self.features)
-		self.objectDetected = True
 		self.tracks   = [[p] for p in self.prev_features.reshape((-1,2))]
 		
+		self.prev_thresh = self.thresh
+		self.prev_gray   = self.gray
 		self.prev_drawing = self.drawing
 		# cv2.imshow('test', self.prev_drawing)
 		
-		self.mask = np.zeros_like(self.frame)
-		
+		self.mask        = np.zeros_like(self.frame)
 		
 		self.draw()
+		
+
 
 	def bgThreshold(self):
-		"""Main operation that runs thresholding
-		using HSV values and background subtraction"""
 
-		#  Apply smoothening filter over the frame
-		self.img = cv2.bilateralFilter(self.frame, 5, 50, 100)
-
-		# If background not captured, run background subtractor MOG2
-		if self.isBgCaptured == 0:
+		#  Main operation
+		self.img = cv2.bilateralFilter(self.frame, 5, 50, 100)  # smoothening filter
+		if self.isBgCaptured == 0:  # this part wont run until background captured
 			self.bgModel = cv2.createBackgroundSubtractorMOG2(600, self.bgSubThreshold, False)
-			self.isBgCaptured = 1    # Updates that background has been captured
+			self.isBgCaptured = 1
 			print( 'Background Captured')
-
-		# Set the HSV values
-		h_low  = 0
-		s_low  = 0
-		v_low  = 103
-		h_high = 61
-		s_high = 65
-		v_high = 255
-
-		# If object (drone) has been detected, create a mask to focus
-		# on object with square of side (85 x 85) pixels, then creates
-		# new image of frame in HSV with clearer image (parameters of 
-		# thresholding and removes BG
-		if self.objectDetected:
-			self.object_mask = np.zeros_like(self.frame)
-			x1 = self.x1 - 85
-			y1 = self.y1 - 85
-			x2 = self.x2 + 85
-			y2 = self.y2 + 85
-			cv2.rectangle(self.object_mask, (x1, y1), (x2, y2), (255,0,0), -1)
-			
-			# Update HSV value if object found
-			h_low  = 0
-			s_low  = 0
-			v_low  = 0
-			h_high = 180
-			s_high = 76
-			v_high = 255
+		# cv2.rectangle(self.img, (int(1 * self.img.shape[1]), (int(0.5 * self.img.shape[1]))),
+        #          (self.img.shape[1], int(1 * self.img.shape[0])), (255, 0, 0), 2) #drawing ROI
+		# cv2.imshow('original', self.img)
+		self.img = self.removeBG(self.img)
+		# img = img[0:int(0.5 * 50),
+		# 	0:self.frame.shape[1]]  # clip the ROI
 		
-		# If object not detected, 
-		img_hsv = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
-		img_blur = cv2.GaussianBlur(img_hsv, (self.blurValue, self.blurValue), 0)
+
+		# cv2.imshow('mask', img)
+
+		# convert the image into binary image
+		self.gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+		self.blur = cv2.GaussianBlur(self.gray, (self.blurValue, self.blurValue), 0)
+		# cv2.imshow('blur', self.blur)
+		ret, self.thresh = cv2.threshold(self.blur, self.threshold, 255, cv2.THRESH_BINARY) #thresholding the frame
+		cv2.imshow('thresh', self.thresh)
 		
-		frame_threshold = cv2.inRange(img_blur, (h_low, s_low, v_low), (h_high, s_high, v_high))
-		frame_threshold = self.removeBG(frame_threshold)
 
-		# cv2.imshow('th', frame_threshold)
+		# get the contours
+		self.thresh1 = copy.deepcopy(self.thresh)
+		self.contours, hierarchy = cv2.findContours(self.thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) #detecting contours
+		length = len(self.contours)
+		maxArea = -1
+		if length > 0:
+			for i in range(length):  # find the biggest contour (according to area)
+				temp = self.contours[i]
+				area = cv2.contourArea(temp)
+				if area > maxArea:
+					maxArea = area
+					ci = i
 
-		drawing = self.img.copy()
-		drawing[frame_threshold == 0] = 0
-		# cv2.drawContours(drawing, [res], 0, (255, 255, 255), thickness=-1)
-		self.drawing = cv2.cvtColor(drawing, cv2.COLOR_BGR2GRAY)
+			res = self.contours[ci]
+			drawing = np.zeros_like(self.frame)
+			cv2.drawContours(drawing, [res], 0, (255, 255, 255), thickness=-1) #drawing contours
+			self.drawing = cv2.cvtColor(drawing, cv2.COLOR_BGR2GRAY)
+			# cv2.imshow('draw', self.drawing)
 			
 
 	def removeBG(self, frame): #Subtracting the background
@@ -145,14 +133,12 @@ class LKTracker(object):
 	def checkPoints(self):
 		"""Re-detect 'Good features to track' (corners)
 		in current frame using sub-pixel accuracy"""
-		self.color = np.random.randint(0,255,(100,3))
-
+		
 		# # Search for good points
-		if not self.objectDetected:
-			self.features = cv2.goodFeaturesToTrack(self.drawing, **feature_params, mask=self.mask)
+		self.features = cv2.goodFeaturesToTrack(self.drawing, **feature_params)
 
-			# Refine the corner locations
-			cv2.cornerSubPix(self.drawing, self.features, **subpix_params)
+		# Refine the corner locations
+		cv2.cornerSubPix(self.drawing, self.features, **subpix_params)
 
 		# self.tracks = [[p] for p in self.features.reshape((-1,2))]
 		
@@ -166,8 +152,8 @@ class LKTracker(object):
 		# self.bgThreshold()
 
 		"""To be used later, re-checks detection every few frames"""
-		if self.current_frame % self.detect_interval == 0:
-			self.checkPoints()
+		# if self.current_frame % self.detect_interval == 0:
+		# 	self.checkPoints()
 
 		
 
@@ -176,18 +162,28 @@ class LKTracker(object):
 
 		self.prev_features = self.features
 		# Calculate optical flow
-		self.bgThreshold()
-		self.features, status, track_error = cv2.calcOpticalFlowPyrLK(self.prev_drawing, self.drawing, tmp, None, **lk_params)
+		# self.features, status, track_error = cv2.calcOpticalFlowPyrLK(self.prev_thresh, self.thresh, tmp, None, **lk_params)
+		if self.current_frame < 10:
+			self.bgThreshold()
+			print(self.features)
+			self.features, status, track_error = cv2.calcOpticalFlowPyrLK(self.prev_drawing, self.drawing, tmp, None, **lk_params)
+
+		elif self.current_frame == 10:
+			self.features, status, track_error = cv2.calcOpticalFlowPyrLK(self.prev_drawing, self.gray, tmp, None, **lk_params)
+			
+		elif self.current_frame > 10:
+			self.features, status, track_error = cv2.calcOpticalFlowPyrLK(self.prev_gray, self.gray, tmp, None, **lk_params)
+			# self.draw()
 
 		# flow = cv2.calcOpticalFlowFarneback(self.prev_gray,self.gray,None,0.5,3,15,3,5,1.2,0)
 
 		self.current_x = (self.features[0][0][0] - self.prev_features[0][0][0]) / (1/self.fps)
 		self.current_y = -1*(self.features[0][0][1] - self.prev_features[0][0][1]) / (1/self.fps)
 		self.draw()
+		self.prev_thresh = self.thresh
+		self.prev_gray   = self.gray
 		self.prev_drawing = self.drawing
 		self.current_frame += 1
-		cv2.imshow("input", self.drawing)
-		# cv2.imshow("mask", self.object_mask)
 		
 
 	def draw(self):
@@ -196,44 +192,26 @@ class LKTracker(object):
 
 		# Draw points as green circles
 		# for point in self.features:
-		self.x1 = 0
-		self.y1 = 0
-		self.x2 = 0
-		self.y2 = 0
 		# 	self.out = cv2.circle(self.frame, (int(point[0][0]), int(point[0][1])), 3, (0, 255, 0), -1)
+
 		for i,(new,old) in enumerate(zip(self.features,self.prev_features)):
 			a,b = new.ravel()
 			c,d = old.ravel()
-			self.x1 += a
-			self.y1 += b
-			self.x2 += c
-			self.y2 += d
-		if i > 0:
-			self.x1 = int(self.x1 / (i+1))
-			self.y1 = int(self.y1 / (i+1))
-			self.x2 = int(self.x2 / (i+1))
-			self.y2 = int(self.y2 / (i+1))
-		else:
-			self.x1 = int(self.x1)
-			self.y1 = int(self.y1)
-			self.x2 = int(self.x2)
-			self.y2 = int(self.y2)
-
-		self.mask  = cv2.line(self.mask, (self.x1,self.y1),(self.x2,self.y2), self.color[i].tolist(), 2)
-		self.frame = cv2.circle(self.frame,(self.x1,self.y1),5,self.color[i].tolist(),-1)
+			self.mask  = cv2.line(self.mask, (a,b),(c,d), color[i].tolist(), 2)
+			self.frame = cv2.circle(self.frame,(a,b),5,color[i].tolist(),-1)
 
 		self.out = cv2.add(self.frame,self.mask)
-		cv2.putText(self.out, 'x = '+ str(self.current_x), (self.x1+20, self.y1-15),
+		cv2.putText(self.out, 'x = '+ str(self.current_x), (int(self.features[0][0][0]+20), int(self.features[0][0][1]-15)),
                 cv2.FONT_HERSHEY_PLAIN, 1 , (0,0,0), thickness=2)
-		cv2.putText(self.out, 'y = ' + str(self.current_y), (self.x1+20, self.y1),
+		cv2.putText(self.out, 'y = ' + str(self.current_y), (int(self.features[0][0][0]+20), int(self.features[0][0][1])),
                 cv2.FONT_HERSHEY_PLAIN, 1 , (0,0,0), thickness=2)
-		cv2.putText(self.out, 'z = ?', (self.x1+20, self.y1+15),
+		cv2.putText(self.out, 'z = ?', (int(self.features[0][0][0]+20), int(self.features[0][0][1]+15)),
                 cv2.FONT_HERSHEY_PLAIN, 1 , (0,0,0), thickness=2)
-		cv2.putText(self.out, 't = ' + str(self.current_frame/self.fps), (self.x1+20, self.y1+30),
+		cv2.putText(self.out, 't = ' + str(self.current_frame/self.fps), (int(self.features[0][0][0]+20), int(self.features[0][0][1]+30)),
                 cv2.FONT_HERSHEY_PLAIN, 1 , (0,0,0), thickness=2)
-		self.writer.write(self.out)
-		cv2.imshow('LKtrack', self.out)
-		# print(self.fps)
+		# self.writer.write(self.out)
+		# cv2.imshow('LKtrack', self.out)
+		print(self.fps)
 
 		
 		
